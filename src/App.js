90310ba61c66f23766c6261f1fb5a30b6046d754
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-02-15 08:48:14 -0800

    created feedback component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import FeedbackItem from './components/FeedbackItem'
+import Header from './components/Header'
+
 function App() {
   return (
-    <h1>Hello from the app component</h1>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
   )
 }
 
